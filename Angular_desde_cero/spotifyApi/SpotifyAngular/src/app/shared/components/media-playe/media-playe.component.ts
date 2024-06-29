9430999f3b71c89d2d05dd735d69085a7adebef1
wilson22cc <131506264+wilson22cc@users.noreply.github.com>
2024-06-29 17:17:47 -0500

    COMMIT2906

--- Angular_desde_cero/spotifyApi/SpotifyAngular/src/app/shared/components/media-playe/media-playe.component.ts
+++ Angular_desde_cero/spotifyApi/SpotifyAngular/src/app/shared/components/media-playe/media-playe.component.ts
@@ -1,4 +1,4 @@
-import { OnDestroy, OnInit } from '@angular/core';
+import { ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { TrackModel } from '../../../core/models/tracks.models';
@@ -10,6 +10,7 @@ import { MultimediaService } from '../../services/multimedia.service';
   styleUrl: './media-playe.component.css'
 })
 export class MediaPlayeComponent implements OnInit, OnDestroy {
+  @ViewChild('progressBar') progressBar: ElementRef = new ElementRef('')
   listObservers$: Array<Subscription> = []
   state: string = 'paused'
   constructor(public multimediaService: MultimediaService) { }
@@ -19,6 +20,15 @@ export class MediaPlayeComponent implements OnInit, OnDestroy {
   }
   ngOnDestroy(): void {
     this.listObservers$.forEach(u => u.unsubscribe())
+  }
+  handlePosition(event: MouseEvent): void {
+    const elNative: HTMLElement = this.progressBar.nativeElement
+    const { clientX } = event
+    const { x, width } = elNative.getBoundingClientRect()
+    const clickX = clientX - x // 1050 - x
+    const persentageFromX = (clickX * 100) / width
+    console.log(`Click(x) : ${persentageFromX}`);
+    this.multimediaService.seekAudio(persentageFromX)
   }
 }
 
--- Angular_desde_cero/spotifyApi/SpotifyAngular/src/app/shared/services/multimedia.service.ts
+++ Angular_desde_cero/spotifyApi/SpotifyAngular/src/app/shared/services/multimedia.service.ts
@@ -12,7 +12,7 @@ export class MultimediaService {
   public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00:00')
   public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject('-00:00')
   public playerStatus$: BehaviorSubject<string> = new BehaviorSubject('paused')
-
+  public playerPercentage$: BehaviorSubject<number> = new BehaviorSubject(0)
   constructor() {
     this.audio = new Audio()
     this.trackInfo$.subscribe(responseOK => {
@@ -23,7 +23,10 @@ export class MultimediaService {
 
     })
     this.listenAllEvent() // se llama a funcuion definida
+
+
   }
+
   private listenAllEvent(): void {
     this.audio.addEventListener('timeupdate', this.calculateTime, false)
     this.audio.addEventListener('playing', this.setPlayerStatus, false)
@@ -31,6 +34,7 @@ export class MultimediaService {
     this.audio.addEventListener('pause', this.setPlayerStatus, false)
     this.audio.addEventListener('ended', this.setPlayerStatus, false)
   }
+
   // funciones para escuchar eventos de playeng, play, paused, ended
   // esta funcion captura los eventos.
   private setPlayerStatus = (state: any) => {
@@ -49,7 +53,9 @@ export class MultimediaService {
         this.playerStatus$.next('paused')
         break;
     }
+
   }
+
   // esta es una funcion de flecha. porque el addEventListener pide como segundo parametro yb arrow
   private calculateTime = () => {
     console.log('Disparando evento')
@@ -57,7 +63,17 @@ export class MultimediaService {
     console.table([duration, currentTime]) // para ver en la consola como tabla los datos
     this.setTimeElapsed(currentTime)
     this.setRemaining(currentTime, duration)
+    this.setPercentage(currentTime, duration)
   }
+
+  private setPercentage(currentTime: number, duration: number): void {
+    // duration: 100%. currentTime es lo calcularemos %
+    let percentage = (currentTime * 100) / duration;
+    this.playerPercentage$.next(percentage)
+
+  }
+
+
   private setTimeElapsed(currentTime: number): void {
     let seconds = Math.floor(currentTime % 60) // retorna volor entero de la div entre 60 (segundos)
     let minutes = Math.floor((currentTime / 60) % 60) // nos retorna los minutos
@@ -66,6 +82,7 @@ export class MultimediaService {
     const displayFormat = `${displayMinutes} : ${displaySeconds}`
     this.timeElapsed$.next(displayFormat) // envia los tiempos formateados.
   }
+
   private setRemaining(currentTime: number, duration: number): void {
     let timeLeft = duration - currentTime;
     let seconds = Math.floor(timeLeft % 60) // retorna volor entero de la div entre 60 (segundos)
@@ -76,17 +93,33 @@ export class MultimediaService {
     this.timeRemaining$.next(displayFormat) // envia los tiempos formateados.
 
   }
+
   // definimos la funcion publica audio
   public setAudio(track: TrackModel): void {
     console.log('🐱‍🏍🐱‍🏍🐱‍🏍🐱‍🏍', track.url);
     this.audio.src = track.url
     this.audio.play()
+
   }
 
   public togglePlayer(): void {
     (this.audio.paused) ? this.audio.play() : this.audio.pause()
   }
+
+  public seekAudio(percentage: number): void {
+    const { duration } = this.audio
+    console.log(`Duration : ${duration} ,Percentage : ${percentage} `);
+    // 100% ----> duration
+    // 70% -----> x
+    //indica a cuanto equivale el porcentaje que se indica de una cancion
+    const percentageToSecond = (percentage * duration) / 100
+    console.log(percentageToSecond)
+    this.audio.currentTime = percentageToSecond
+  }
+
 }
+
+
 
 
 
